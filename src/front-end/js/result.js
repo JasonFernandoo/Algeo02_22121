959f43e7b7b09e8d3e0834ee3af98fba9df8dc9d
JasonFernandoo <j450n.fernando@gmail.com>
2023-11-02 17:07:35 +0700

    result tab

---
+++ src/front-end/js/result.js
@@ -0,0 +1,17 @@
+function showResult() {
+    const resultTab = document.getElementById('result-tab');
+    resultTab.classList.add('show');
+}
+
+document.addEventListener('click', function(event) {
+    const resultTab = document.getElementById('result-tab');
+    const button = document.getElementById('search');
+
+    if (event.target === button) {
+        // Clicking the "Search" button should show the result-tab, but don't hide it.
+        showResult();
+    } else if (event.target !== resultTab && !resultTab.contains(event.target)) {
+        // Clicking outside of the result-tab hides it.
+        resultTab.classList.remove('show');
+    }
+});
