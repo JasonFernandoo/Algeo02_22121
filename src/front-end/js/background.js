9156cbe0a27cf5a62492a301654296b2f050009b
JasonFernandoo <j450n.fernando@gmail.com>
2023-11-09 12:46:19 +0700

    renew design

--- src/front-end/js/background.js
+++ src/front-end/js/background.js
@@ -1,6 +1,6 @@
 const bg = document.getElementById('home');
 
 window.addEventListener('scroll', function(){
-    bg.style.backgroundSize = 160 - +window.scrollY / 12 + '%';
+    bg.style.backgroundSize = 110 - +window.scrollY / 57 + '%';
     bg.style.opacity = 1 - +window.scrollY / 700 + '';
 })
