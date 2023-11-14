84b4ed70b25652f3702e3c6917b9109b9825d922
jojogantengbanget <jonathan.srgh@gmail.com>
2023-11-14 20:04:51 +0700

    udah nii texture sama color kenceng 4 detik

--- src/back-end/CBIR/color/driver_color.py
+++ src/back-end/CBIR/color/driver_color.py
@@ -1,3 +1,5 @@
+import concurrent.futures
+import cProfile
 import cv2
 import numpy as np
 import os
@@ -12,11 +14,17 @@ class ImageComparator:
         self.histogram_cache = {}
 
     @lru_cache(maxsize=None)
-    def calculate_histograms(self, image_path):
+    def calculate_histograms(self, image_path, resize=True, target_size=(64, 64)):
         if image_path in self.histogram_cache:
             return self.histogram_cache[image_path]
+
         image = cv2.imread(image_path)
-        hist = cv2.calcHist([image], [0, 1, 2], None, [8, 12, 3], [0, 180, 0, 256, 0, 256])
+
+        if resize:
+            image = cv2.resize(image, target_size)
+
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        hist = cv2.calcHist([hsv_image], [0, 1, 2], None, [8, 12, 3], [0, 180, 0, 256, 0, 256])
         self.histogram_cache[image_path] = hist
         return hist
 
@@ -29,58 +37,72 @@ class ImageComparator:
         similarity = dot_product / (magnitude1 * magnitude2)
         return similarity
 
+    def process_image(self, image2_path):
+        hist2 = self.calculate_histograms(image2_path)
+        similarity = self.calculate_cosine_similarity(hist1, hist2)
+        return image2_path, similarity
+
     def compare_images_in_folder(self, folder_path, save_path=None):
+        global hist1
+        hist1 = self.calculate_histograms(self.image1_path)
+
         similar_images = []
         similar_percent = []
         most_similar_images = []
-        highest_similarity = -1  # Initialize with a low value
+        highest_similarity = -1
 
-        hist1 = self.calculate_histograms(self.image1_path)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.process_image, os.path.join(folder_path, filename))
+                       for filename in os.listdir(folder_path)
+                       if filename.endswith(('.jpg', '.jpeg', '.png'))]
 
-        for filename in os.listdir(folder_path):
-            if filename.endswith(('.jpg', '.jpeg', '.png')):
-                image2_path = os.path.join(folder_path, filename)
-
-                if image2_path in self.histogram_cache:
-                    hist2 = self.histogram_cache[image2_path]
-                else:
-                    hist2 = self.calculate_histograms(image2_path)
-                    self.histogram_cache[image2_path] = hist2
-
-                similarity = self.calculate_cosine_similarity(hist1, hist2)
+            for future in concurrent.futures.as_completed(futures):
+                image2_path, similarity = future.result()
 
                 if similarity > 0.6:
-                    similar_images.append(image2_path)
+                    similar_images.append((image2_path, similarity))
                     similar_percent.append(similarity)
 
                 if similarity > highest_similarity:
                     highest_similarity = similarity
-                    most_similar_images = [image2_path]
+                    most_similar_images = [(image2_path, similarity)]
                 elif similarity == highest_similarity:
-                    most_similar_images.append(image2_path)
+                    most_similar_images.append((image2_path, similarity))
+
+        # Sort similar images by similarity (high to low)
+        similar_images.sort(key=lambda x: x[1], reverse=True)
 
         i = 0
         if save_path and similar_images:
             with open(save_path, 'w') as file:
-                for path in similar_images:
-                    file.write(path + '\n' + "cosine similarity: " + str(round(similar_percent[i] * 100, 2)) + '%' + '\n')
+                for path, sim in similar_images:
+                    file.write(path + '\n' + "cosine similarity: " + str(round(sim * 100, 2)) + '%' + '\n')
                     i += 1
 
         return most_similar_images, highest_similarity
 
 if __name__ == "__main__":
-    image1_path = 'C:/Users/attar/OneDrive/Documents/GitHub/Algeo02_22121/src/back-end/image/search/105.jpg'  # Gantilah dengan jalur lengkap menuju gambar Anda
-    image_folder = 'C:/Users/attar/OneDrive/Documents/GitHub/Algeo02_22121/src/back-end/image/dataset'  # Gantilah dengan jalur lengkap ke folder yang berisi gambar-gambar
-    save_path = 'C:/Users/attar/OneDrive/Documents/GitHub/Algeo02_22121/src/back-end/CBIR/color/similar_color.txt'  # Gantilah dengan jalur lengkap file penyimpanan hasil
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    image1_path = 'C:/Users/jonat/Documents/Koding Santuy/Algeoasik/Algeo02_22121/src/back-end/image/search/105.jpg'
+    image_folder = 'C:/Users/jonat/Documents/Koding Santuy/Algeoasik/Algeo02_22121/src/back-end/image/dataset'
+    save_path = 'C:/Users/jonat/Documents/Koding Santuy/Algeoasik/Algeo02_22121/src/back-end/CBIR/color/similar_color.txt' 
 
     comparator = ImageComparator(image1_path)
     similar_images, highest_similarity = comparator.compare_images_in_folder(image_folder, save_path)
 
     if similar_images:
-        print("Most similar images:", similar_images)
-        print("Cosine Similarity:", highest_similarity * 100, "%")
+        print("Most similar images:")
+        for path, sim in similar_images:
+            print(path, "Cosine Similarity:", sim * 100, "%")
     else:
         print("No similar images found with similarity above 0.6.")
-end_time = time.time()
-execution_time = end_time - start_time
-print("Waktu eksekusi:", execution_time, "detik")
+
+    # Stop profiling and print the results
+    profiler.disable()
+    profiler.print_stats(sort='cumulative')
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print("Waktu eksekusi:", execution_time, "detik")
--- src/back-end/CBIR/texture/driver_texture.py
+++ src/back-end/CBIR/texture/driver_texture.py
@@ -1,131 +1,10 @@
-# import cv2
-# import numpy as np
-# import time
-# from numba import jit
-# import os
-
-# start_time = time.time()
-
-# @jit(nopython=True)
-# def calculate_co_occurrence_matrix(image, offset, levels=16):
-#     # Create a zero-initialized Co-occurrence Matrix
-#     co_occurrence_matrix = np.zeros((levels, levels))
-
-#     # Get image dimensions
-#     height, width = image.shape
-
-#     for y in range(height):
-#         for x in range(width):
-#             x2 = x + offset[0]
-#             y2 = y + offset[1]
-
-#             if 0 <= x2 < width and 0 <= y2 < height:
-#                 i = image[y, x]
-#                 j = image[y2, x2]
-#                 co_occurrence_matrix[i, j] += 1
-
-#     # Normalize the co-occurrence matrix
-#     co_occurrence_matrix /= co_occurrence_matrix.sum()
-
-#     return co_occurrence_matrix
-
-# @jit(nopython=True)
-# def calculate_contrast(co_occurrence_matrix):
-#     # Calculate contrast from the co-occurrence matrix
-#     levels = co_occurrence_matrix.shape[0]
-#     mean = np.arange(levels).mean()
-#     contrast = np.sum((np.arange(levels) - mean) ** 2 * co_occurrence_matrix)
-#     return contrast
-
-# @jit(nopython=True)
-# def calculate_homogeneity(co_occurrence_matrix):
-#     # Calculate homogeneity from the co-occurrence matrix
-#     levels = co_occurrence_matrix.shape[0]
-#     mean = np.arange(levels).mean()
-#     homogeneity = np.sum(co_occurrence_matrix / (1 + np.abs(np.arange(levels) - mean)))
-#     return homogeneity
-
-# @jit(nopython=True)
-# def calculate_entropy(co_occurrence_matrix):
-#     # Calculate entropy from the co-occurrence matrix
-#     eps = 1e-10  # Small constant to avoid log(0)
-#     entropy = -np.sum(co_occurrence_matrix * np.log2(co_occurrence_matrix + eps))
-#     return entropy
-
-# class TextureCBIR:
-#     def __init__(self, image1_path):
-#         self.image1 = cv2.imread(image1_path, cv2.IMREAD_GRAYSCALE)
-#         self.image1 = self.quantize_image(self.image1)
-
-#     def quantize_image(self, image, levels=16):
-#         # Normalize the pixel values to the range [0, levels-1]
-#         image = cv2.normalize(image, None, 0, levels - 1, cv2.NORM_MINMAX)
-#         return image.astype(np.uint8)
-
-#     def extract_texture_features(self, image, levels=16):
-#         offsets = [(1, 0), (1, 1), (0, 1), (-1, 1)]
-#         features = []
-
-#         for offset in offsets:
-#             co_occurrence_matrix = calculate_co_occurrence_matrix(image, offset, levels)
-#             contrast = calculate_contrast(co_occurrence_matrix)
-#             homogeneity = calculate_homogeneity(co_occurrence_matrix)
-#             entropy = calculate_entropy(co_occurrence_matrix)
-#             features.extend([contrast, homogeneity, entropy])
-
-#         return features
-
-#     def compare_images(self, image2_path):
-#         image2 = cv2.imread(image2_path, cv2.IMREAD_GRAYSCALE)
-#         image2 = self.quantize_image(image2)
-
-#         texture_features1 = self.extract_texture_features(self.image1)
-#         texture_features2 = self.extract_texture_features(image2)
-
-#         # Calculate Cosine Similarity between the two texture feature vectors
-#         dot_product = np.dot(texture_features1, texture_features2)
-#         magnitude1 = np.linalg.norm(texture_features1)
-#         magnitude2 = np.linalg.norm(texture_features2)
-#         similarity = dot_product / (magnitude1 * magnitude2)
-#         return similarity
-
-#     def compare_images_in_folder(self, folder_path, save_path):
-#         similar_images = []
-
-#         for filename in os.listdir(folder_path):
-#             if filename.endswith(('.jpg', '.jpeg', '.png')):
-#                 image2_path = os.path.join(folder_path, filename)
-#                 similarity = self.compare_images(image2_path)
-#                 if similarity > 0.6:  # Set similarity threshold here
-#                     similar_images.append((image2_path, similarity))
-
-#         if save_path and similar_images:
-#             with open(save_path, 'w') as file:
-#                 for path, sim in similar_images:
-#                     file.write(f"{path}\nCosine Similarity: {round(sim * 100, 2)}%\n")
-
-#         return similar_images
-
-# if __name__ == "__main__":
-#     image1_path = 'C:/Users/attar/OneDrive/Documents/GitHub/Algeo02_22121/src/back-end/image/search/105.jpg'  # Gantilah dengan jalur lengkap menuju gambar pertama
-#     image_folder = 'C:/Users/attar/OneDrive/Documents/GitHub/Algeo02_22121/src/back-end/image/dataset'  # Gantilah dengan jalur lengkap ke folder gambar
-#     save_path = 'C:/Users/attar/OneDrive/Documents/GitHub/Algeo02_22121/src/back-end/CBIR/texture/similar_texture.txt'  # Gantilah dengan jalur lengkap file penyimpanan hasil
-
-#     cbir = TextureCBIR(image1_path)
-#     similar_images = cbir.compare_images_in_folder(image_folder, save_path)
-
-#     if similar_images:
-#         print("Similar images found. Check the output file for details.")
-#     else:
-#         print("No similar images found.")
-
-#     end_time = time.time()
-#     print("Execution Time:", end_time - start_time, "seconds")
 import cv2
 import numpy as np
-import time
-from numba import jit
 import os
+import cProfile
+from numba import jit
+from concurrent.futures import ThreadPoolExecutor
+import time
 from functools import lru_cache
 
 start_time = time.time()
@@ -170,11 +49,11 @@ def calculate_entropy(co_occurrence_matrix):
 
 class TextureCBIR:
     def __init__(self, image1_path):
-        self.image1 = cv2.imread(image1_path, cv2.IMREAD_GRAYSCALE)
-        self.image1 = self.quantize_image(self.image1)
-        self.histogram_cache = {}
+        self.image1 = self.load_and_quantize_image(image1_path)
 
-    def quantize_image(self, image, levels=16):
+    def load_and_quantize_image(self, image_path, levels=16):
+        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        image = cv2.resize(image, (64, 64))  # Resize the image to 64x64
         image = cv2.normalize(image, None, 0, levels - 1, cv2.NORM_MINMAX)
         return image.astype(np.uint8)
 
@@ -191,21 +70,13 @@ class TextureCBIR:
 
         return features
 
-    @lru_cache(maxsize=None)
+    @lru_cache(maxsize=128)
     def calculate_histogram(self, image_path):
-        if image_path in self.histogram_cache:
-            return self.histogram_cache[image_path]
-
-        image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        image = self.quantize_image(image)
+        image = self.load_and_quantize_image(image_path)
         hist = self.extract_texture_features(image)
-        self.histogram_cache[image_path] = hist
         return hist
 
     def compare_images(self, image2_path):
-        image2 = cv2.imread(image2_path, cv2.IMREAD_GRAYSCALE)
-        image2 = self.quantize_image(image2)
-
         texture_features1 = self.extract_texture_features(self.image1)
         texture_features2 = self.calculate_histogram(image2_path)
 
@@ -216,14 +87,12 @@ class TextureCBIR:
         return similarity
 
     def compare_images_in_folder(self, folder_path, save_path):
-        similar_images = []
+        image_paths = [os.path.join(folder_path, filename) for filename in os.listdir(folder_path) if filename.endswith(('.jpg', '.jpeg', '.png'))]
 
-        for filename in os.listdir(folder_path):
-            if filename.endswith(('.jpg', '.jpeg', '.png')):
-                image2_path = os.path.join(folder_path, filename)
-                similarity = self.compare_images(image2_path)
-                if similarity > 0.6:
-                    similar_images.append((image2_path, similarity))
+        with ThreadPoolExecutor(max_workers=8) as executor:
+            results = list(executor.map(self.compare_images, image_paths))
+
+        similar_images = [(path, sim) for path, sim in zip(image_paths, results) if sim > 0.6]
 
         if save_path and similar_images:
             with open(save_path, 'w') as file:
@@ -233,17 +102,23 @@ class TextureCBIR:
         return similar_images
 
 if __name__ == "__main__":
-    image1_path = 'C:/Users/attar/OneDrive/Documents/GitHub/Algeo02_22121/src/back-end/image/search/105.jpg'
-    image_folder = 'C:/Users/attar/OneDrive/Documents/GitHub/Algeo02_22121/src/back-end/image/dataset'
-    save_path = 'C:/Users/attar/OneDrive/Documents/GitHub/Algeo02_22121/src/back-end/CBIR/texture/similar_texture.txt'
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    image1_path = 'C:/Users/jonat/Documents/Koding Santuy/Algeoasik/Algeo02_22121/src/back-end/image/search/105.jpg'
+    image_folder = 'C:/Users/jonat/Documents/Koding Santuy/Algeoasik/Algeo02_22121/src/back-end/image/dataset'
+    save_path = 'C:/Users/jonat/Documents/Koding Santuy/Algeoasik/Algeo02_22121/src/back-end/CBIR/texture/similar_texture.txt' 
 
     cbir = TextureCBIR(image1_path)
     similar_images = cbir.compare_images_in_folder(image_folder, save_path)
-
+    
     if similar_images:
         print("Similar images found. Check the output file for details.")
     else:
         print("No similar images found.")
+
+    profiler.disable()
+    profiler.print_stats()
 
     end_time = time.time()
     print("Execution Time:", end_time - start_time, "seconds")
